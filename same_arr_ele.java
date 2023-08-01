101d4b9bad6b0f08fd81c3649ca95e1b4ef0eb84
Vishal Nikam <125108007+Vishal-1524@users.noreply.github.com>
2023-08-01 16:38:03 +0530

    Add files via upload

---
+++ same_arr_ele.java
@@ -0,0 +1,18 @@
+ package practice;
+
+import java.util.Arrays;
+
+public class same_arr_ele 
+{
+ public static void main(String[] args) 
+ {
+	int arr[]= {1,2,3};
+	int arr1[]={1,2,3};
+	System.out.println(Arrays.equals(arr,arr1));
+	System.out.println(arr);
+
+	System.out.println(arr1);
+
+	
+ }
+}
